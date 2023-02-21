e60e96afaa894cb222d464078937fc759f468d02
BakeryAyesha <ayeshasiddiquaa623@gmail.com>
2023-02-21 14:52:38 +0530

    message is committed

---
+++ Assignment1_1/Assignment1_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assignment1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the operand");
+            string operand = Console.ReadLine();
+            switch(operand)
+            {
+                case "+":
+                    int result = num1 + num2;
+                    Console.WriteLine("Addition :" +result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine("Substraction :" + result);
+                    break;
+                case "*":
+                     result = num1 * num2;
+                    Console.WriteLine("Multiplication :" + result);
+                    break;
+                case "/":
+                     result = num1 / num2;
+                    Console.WriteLine("Division :" + result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operations");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment1_4/Assignment1_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_4
+{
+    class swap
+    {
+        static void Swap(int a1, int b1)
+        {
+            a1 = (a1 & b1) + (a1 | b1);
+            b1 = a1 + (~b1) + 1;
+            a1 = a1 + (~b1) + 1;
+            Console.WriteLine("After Swap : " + a1 + "," + b1);
+        }
+        static void Main()
+        {
+            int a1 = 15;
+            int b1 = 10;
+            Swap(a1, b1);
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
